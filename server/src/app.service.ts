5803602bd307d786937253e923ad93d067b418bf
ThyuHtooAungOwO <thyuhtoo7@gmail.com>
2026-01-06 22:24:44 +0630

    updated the api running text

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World! Here's is the menu";
+    return 'Dessert menu API is running.';
   }
 }
