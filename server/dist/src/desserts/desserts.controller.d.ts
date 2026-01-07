a6429d414d0849a4d4c1b170b35e8cca9c04528e
ThyuHtooAungOwO <thyuhtoo7@gmail.com>
2026-01-08 02:22:01 +0630

    Cleanup: remove ignored files like node_modules and .env

--- server/dist/src/desserts/desserts.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { DessertsService } from './desserts.service';
-export declare class DessertsController {
-    private readonly dessertsService;
-    constructor(dessertsService: DessertsService);
-    getDesserts(): Promise<import("./dto/desserts.dto").DessertDto[]>;
-}
